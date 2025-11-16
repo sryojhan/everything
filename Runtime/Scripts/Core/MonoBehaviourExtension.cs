3f07016aea8c77a5f3a4cde739b671ada7092507
Yojhan <sryojhan@gmail.com>
2025-11-16 17:00:42 +0100

    MonoBehaviour extensions!

---
+++ Runtime/Scripts/Core/MonoBehaviourExtension.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public static class MonobehaviourExtension
+{
+    public static T GetOrAddComponent<T>(this MonoBehaviour obj) where T : MonoBehaviour
+    {
+        T comp = obj.GetComponent<T>();
+
+        if (!comp) comp = obj.gameObject.AddComponent<T>();
+
+        return comp;
+    }
+}
